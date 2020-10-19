7f359c9bad83f75e1fbe937b44c0b057446bbfe4
Santiago Balaguer <santi@Santiagos-MacBook-Pro.local>
2020-10-19 21:23:29 +0200

    changed services connection

--- client/src/Services/api-calls.js
+++ client/src/Services/api-calls.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
 import contractData from '../Static/contractInfo.json'
 
+const api = axios.create({
+    baseURL: process.env.REACT_APP_BACKEND_URL,
+    withCredentials: true
+  });
+
 export const getData = async (name) =>{
+    console.log(process.env.REACT_APP_BACKEND_URL)
     try{
-        const response = await axios.post(`${process.env.BACKEND_URL}/gas`,{[name]:contractData[name]})
+        const response = await api.post(`/gas`,{[name]:contractData[name]})
         return response
     }catch(error){
         console.log(error)
