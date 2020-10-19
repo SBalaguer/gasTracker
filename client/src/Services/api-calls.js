f631116942e8a049af1ac0b96949c9e3e22fec55
Santiago Balaguer <santi@Santiagos-MacBook-Pro.local>
2020-10-19 21:07:54 +0200

    added backend url env var

--- client/src/Services/api-calls.js
+++ client/src/Services/api-calls.js
@@ -3,7 +3,7 @@ import contractData from '../Static/contractInfo.json'
 
 export const getData = async (name) =>{
     try{
-        const response = await axios.post('http://localhost:5000/gas',{[name]:contractData[name]})
+        const response = await axios.post(`${process.env.BACKEND_URL}/gas`,{[name]:contractData[name]})
         return response
     }catch(error){
         console.log(error)
